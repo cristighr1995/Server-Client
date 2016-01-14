c7e779d5b98d4b35cca8195c023dc887d8b4569e
cristighr1995 <cristi.gherghina_1995@yahoo.com>
2016-01-14 16:00:14 +0200

    Add Client Part and Server Part

---
+++ ClientPart/ClientPart/Program.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+// State object for receiving data from remote device.
+public class StateObject
+{
+    // Client socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 256;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+}
+
+public class AsynchronousClient
+{
+    // The port number for the remote device.
+    private const int port = 11000;
+
+    // ManualResetEvent instances signal completion.
+    private static ManualResetEvent connectDone =
+        new ManualResetEvent(false);
+    private static ManualResetEvent sendDone =
+        new ManualResetEvent(false);
+    private static ManualResetEvent receiveDone =
+        new ManualResetEvent(false);
+
+    // The response from the remote device.
+    private static String response = String.Empty;
+    private static Socket Client;
+    private static IPAddress recivedIP;
+    private static IPAddress ownServerIP = IPAddress.Parse("169.254.80.80"); 
+    private static string serverUID = string.Empty;
+    private static string flagID = string.Empty;
+    private static bool recivedConfirmationForFlag = false;
+
+    private static void StartClient()
+    {
+        // Connect to a remote device.
+        try
+        {
+            // Establish the remote endpoint for the socket.
+            // The name of the 
+
+            IPAddress ipAddress = ownServerIP;
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+            // Create a TCP/IP socket.
+            Client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect to the remote endpoint.
+            Client.BeginConnect(remoteEP,
+                new AsyncCallback(ConnectCallback), Client);
+            connectDone.WaitOne();
+
+            // Send test data to the remote device.
+            Send(Client, "next_server");
+            sendDone.WaitOne();
+
+            // Receive the response from the remote device.
+            Receive(Client);
+            receiveDone.WaitOne();
+
+            // Write the response to the console.
+            Console.WriteLine("Response received : {0}", response);
+            recivedIP = IPAddress.Parse("169.254.80.80");//IPAddress.Parse(response);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            Socket client = (Socket)ar.AsyncState;
+
+            // Complete the connection.
+            client.EndConnect(ar);
+
+           // Console.WriteLine("Socket connected to {0}",
+           //     client.RemoteEndPoint.ToString());
+
+            // Signal that the connection has been made.
+            connectDone.Set();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void Receive(Socket client)
+    {
+        try
+        {
+            // Create the state object.
+            StateObject state = new StateObject();
+            state.workSocket = client;
+
+            // Begin receiving the data from the remote device.
+            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReceiveCallback), state);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the state object and the client socket 
+            // from the asynchronous state object.
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket client = state.workSocket;
+
+            // Read data from the remote device.
+            int bytesRead = client.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There might be more data, so store the data received so far.
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                // Get the rest of the data.
+                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), state);
+            }
+            else
+            {
+                // All the data has arrived; put it in response.
+                if (state.sb.Length > 1)
+                {
+                    response = state.sb.ToString();
+                }
+                // Signal that all bytes have been received.
+                receiveDone.Set();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void Send(Socket client, String data)
+    {
+        // Convert the string data to byte data using ASCII encoding.
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+        // Begin sending the data to the remote device.
+        client.BeginSend(byteData, 0, byteData.Length, 0,
+            new AsyncCallback(SendCallback), client);
+    }
+
+    private static void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            Socket client = (Socket)ar.AsyncState;
+
+            // Complete sending the data to the remote device.
+            int bytesSent = client.EndSend(ar);
+            //Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+            // Signal that all bytes have been sent.
+            sendDone.Set();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void StartClientForRecivedIP()
+    {
+        // Connect to a remote device.
+        try
+        {
+            // Establish the remote endpoint for the socket.
+            // The name of the 
+
+            IPAddress ipAddress = recivedIP;//IPAddress.Parse("169.254.80.80");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+            // Create a TCP/IP socket.
+            Client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect to the remote endpoint.
+            Client.BeginConnect(remoteEP,
+                new AsyncCallback(ConnectCallback), Client);
+            connectDone.WaitOne();
+
+            // Send test data to the remote device.
+            Send(Client, "who_are_you?");
+            sendDone.WaitOne();
+
+            // Receive the response from the remote device.
+            Receive(Client);
+            receiveDone.WaitOne();
+
+            // Write the response to the console.
+            Thread.Sleep(2000);
+            serverUID = response;
+            Console.WriteLine("Response received : {0}", response);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void AskClientForFlag()
+    {
+        try
+        {
+
+            IPAddress ipAddress = recivedIP;
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+            Client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            Client.BeginConnect(remoteEP,
+                new AsyncCallback(ConnectCallback), Client);
+            connectDone.WaitOne();
+            Send(Client, "have_flag?");
+            sendDone.WaitOne();
+            Receive(Client);
+            receiveDone.WaitOne();
+            Thread.Sleep(2000);
+            if (response.Equals("NO"))
+                recivedConfirmationForFlag = false;
+            else
+                recivedConfirmationForFlag = true;
+            if (recivedConfirmationForFlag)
+                flagID = response.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1];
+            Console.WriteLine("Response received : {0}", response);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void CaptureFlag()
+    {
+        try
+        {
+            IPAddress ipAddress = recivedIP;
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+            Client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            Client.BeginConnect(remoteEP,
+                new AsyncCallback(ConnectCallback), Client);
+            connectDone.WaitOne();
+            Send(Client, "capture_flag #" + flagID);
+            sendDone.WaitOne();
+            Receive(Client);
+            receiveDone.WaitOne();
+            Thread.Sleep(2000);
+            Console.WriteLine(response);
+            if (response.StartsWith("FLAG"))
+                Console.WriteLine("Got the flag from {0}", serverUID);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void HideFlag()
+    {
+        try
+        {
+            IPAddress ipAddress = ownServerIP;
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+            Client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            Client.BeginConnect(remoteEP,
+                new AsyncCallback(ConnectCallback), Client);
+            connectDone.WaitOne();
+            Send(Client, "hide_flag #hello");
+            sendDone.WaitOne();
+            Receive(Client);
+            receiveDone.WaitOne();
+            Thread.Sleep(2000);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static int Main(String[] args)
+    {
+        while (true)
+        {
+            StartClient();
+            recivedConfirmationForFlag = false;
+            response = string.Empty;
+            serverUID = string.Empty;
+            flagID = string.Empty;
+            StartClientForRecivedIP();
+            AskClientForFlag();
+            if (recivedConfirmationForFlag)
+            {                
+                CaptureFlag();
+                HideFlag();
+                Random r = new Random();
+                int n = r.Next(10);
+                Console.WriteLine(String.Format("resting now for {0} seconds", n));
+                Thread.Sleep(n * 1000);
+                recivedConfirmationForFlag = false;
+            }
+            else
+            {
+                while (!recivedConfirmationForFlag)
+                {
+                    StartClient();
+                    recivedConfirmationForFlag = false;
+                    response = string.Empty;
+                    serverUID = string.Empty;
+                    flagID = string.Empty;
+                    StartClientForRecivedIP();
+                    AskClientForFlag();
+                }
+            }
+        }
+        Console.Read();
+        return 0;
+    }
+}
---
+++ ServerPart/DevAcademyHackathon/Server.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+// State object for reading client data asynchronously
+public class StateObject
+{
+    // Client  socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 1024;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+}
+
+public class AsynchronousSocketListener
+{
+    // Thread signal.
+    public static ManualResetEvent allDone = new ManualResetEvent(false);
+    public static string UID = string.Empty;
+    public static bool hasFlag = true;
+    public static string IP = string.Empty;
+    public static string flagValue = "aaaa";
+    public static string flagID = string.Empty;
+
+    public AsynchronousSocketListener()
+    {
+    }
+
+    public static void StartListening()
+    {
+        // Data buffer for incoming data.
+        byte[] bytes = new Byte[1024];
+
+        // Establish the local endpoint for the socket.
+        // The DNS name of the computer
+
+        IPAddress ipAddress = IPAddress.Parse("188.26.11.125"); //ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 80);
+
+        // Create a TCP/IP socket.
+        Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and listen for incoming connections.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
+
+            while (true)
+            {
+                // Set the event to nonsignaled state.
+                allDone.Reset();
+
+                // Start an asynchronous socket to listen for connections.
+                Console.WriteLine("Waiting for a connection...");
+                listener.BeginAccept(
+                    new AsyncCallback(AcceptCallback),
+                    listener);
+
+                // Wait until a connection is made before continuing.
+                allDone.WaitOne();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static void AcceptCallback(IAsyncResult ar)
+    {
+        // Signal the main thread to continue.
+        allDone.Set();
+
+        // Get the socket that handles the client request.
+        Socket listener = (Socket)ar.AsyncState;
+        Socket handler = listener.EndAccept(ar);
+
+        // Create the state object.
+        StateObject state = new StateObject();
+        state.workSocket = handler;
+        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+            new AsyncCallback(ReadCallback), state);
+    }
+
+    public static void ReadCallback(IAsyncResult ar)
+    {
+        String content = String.Empty;
+
+        // Retrieve the state object and the handler socket
+        // from the asynchronous state object.
+        StateObject state = (StateObject)ar.AsyncState;
+        Socket handler = state.workSocket;
+
+        // Read data from the client socket. 
+        int bytesRead = handler.EndReceive(ar);
+
+        if (bytesRead > 0)
+        {
+            // There  might be more data, so store the data received so far.
+            state.sb.Append(Encoding.ASCII.GetString(
+                state.buffer, 0, bytesRead));
+
+            // Check for end-of-file tag. If it is not there, read 
+            // more data.
+            content = state.sb.ToString();
+            if (content.Equals("who_are_you?"))
+            {
+                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
+                Send(handler, UID);
+            }
+            else if (content.Equals("next_server"))
+            {
+                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                    content.Length, content);
+                generateRandomIP();
+                Send(handler, IP);
+            }
+            else if (content.Equals("have_flag?"))
+            {
+                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                   content.Length, content);
+                string response = string.Empty;
+                string flagId = string.Empty;
+                if (!hasFlag)
+                    response = "NO";
+                else
+                {
+                    response = "YES ";                    
+                    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                    var random = new Random();
+                    for (int i = 0; i < 16; i++)
+                    {
+                        flagId += chars[random.Next(chars.Length)];
+                    }
+                    flagID = flagId;
+                }
+                Send(handler, response + flagId);
+            }
+            else if (content.StartsWith("capture_flag"))
+            {
+                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                   content.Length, content);
+                string response = string.Empty;                
+                if (content.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1].Equals('#' + flagID))
+                {
+                    response += "FLAG:" + flagValue;
+                    hasFlag = false;
+                }
+                else
+                    response += "ERR: You're trying to trick me!";
+                Send(handler, response);
+            }
+            else if (content.StartsWith("hide_flag"))
+            {
+                flagValue = content.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1].Substring(1);
+                hasFlag = true;
+                Send(handler, "");
+            }        
+        }
+    }
+
+    private static void Send(Socket handler, String data)
+    {
+        // Convert the string data to byte data using ASCII encoding.
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+        // Begin sending the data to the remote device.
+        handler.BeginSend(byteData, 0, byteData.Length, 0,
+            new AsyncCallback(SendCallback), handler);
+    }
+
+    private static void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            Socket handler = (Socket)ar.AsyncState;
+
+            // Complete sending the data to the remote device.
+            int bytesSent = handler.EndSend(ar);
+            Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private static void generateUID()
+    {
+        string UIDfirstPart = "emanoloiu.";
+        UID += UIDfirstPart;
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var random = new Random();
+        for (int i = 0; i < 16 - UIDfirstPart.Length; i++)
+        {
+            UID += chars[random.Next(chars.Length)];
+        }
+    }
+
+    private static void generateRandomIP()
+    {
+        Random r = new Random();
+        IP = r.Next(256) + "." + r.Next(256) + "." + r.Next(256) + "." + r.Next(256);
+    }
+
+
+    public static int Main(String[] args)
+    {
+        generateUID();
+        StartListening();
+
+        return 0;
+    }
+}
